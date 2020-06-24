8676ce25ad73bfb24e6d6af5d73cacdb44bd858d
sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-24 09:26:49 +0200

    Add Some Parts

--- Webchat/app.js
+++ Webchat/app.js
@@ -3,33 +3,66 @@ const events = require('events');
 const http = require("http");
 const morgan = require("morgan");
 const helmet = require("helmet")();
+const crypto = require("crypto");
+const hash = crypto.createHash('sha256');
+hash.update(Math.random().toString());
+const secretOfSess = hash.digest('hex');
+const session = require('express-session');
 const app = expr();
 app.use(morgan("tiny"));
 app.use(helmet);
 app.use(expr.static(__dirname + '/p'));
 const changeServ = new events.EventEmitter();
+app.set('trust proxy', 1);
+app.use(session({
+    secret: secretOfSess,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { path: '/about/session', httpOnly: false, secure: false, expires: new Date(Date.now() + 3600000 * 3), maxAge: 3600000*3 }
+}));
 const pars = [
     {
         "path": "/",
-        "file": "index.html"
+        "file": "index.html",
+        "onlyon": false
     },
     {
         "path": "/service/send/",
-        "file": "send.html"
+        "file": "send.html",
+        "onlyon": true
     },
     {
         "file": "clie.html",
-        "path": "/service/rec/"
+        "path": "/service/rec/",
+        "onlyon": false
     }
 ];
+let userhere = false;
 for (let xx of pars) {
     app.get(xx.path, (req, res) => {
-        res.sendFile(`${__dirname}/views/${xx.file}`);
+        if (!xx.onlyon) {
+            res.sendFile(`${__dirname}/views/${xx.file}`);
+        } else {
+            if (!userhere) {
+                res.sendFile(`${__dirname}/views/${xx.file}`);
+            } else {
+                res.sendStatus(423);
+            }
+        }
     });
 }
-let userhere = false;
 app.get("/service/user", (req, res) => {
     res.json({ "serverAvail": !userhere });
+});
+
+app.get("/about/session", (req, res) => {
+    let prevHere = false;
+    if (req.session.prevHere) {
+        prevHere = true;
+    } else {
+        req.session.prevHere = true;
+    }
+    res.json({ prevHere: prevHere, "time": req.session.cookie.maxAge });
 });
 const server = http.createServer(app);
 const io = require('socket.io')(server);
@@ -41,15 +74,18 @@ indexNamespace.on('connection', (socket) => {
         socket.emit("change", !userhere);
     });
 });
+function doOn() {
+    if (!userhere) {
+        userhere = true;
+        changeServ.emit('change');
+    }
+}
 serverNamespace.on('connection', (socket) => {
     socket.emit("welcome", true);
     console.log('a user connected');
     socket.on("imagestream", (data) => {
+        doOn();
         if (data !== "data:,") {
-            if (!userhere) {
-                userhere = true;
-                changeServ.emit('change');
-            }
             clientNamespace.emit("imagestream", data);
         }
     });
--- Webchat/p/index.js
+++ Webchat/p/index.js
@@ -1,8 +1,24 @@
 ﻿$(document).ready(function () {
-    $.get("/service/user", function(data) {
+    $.get("/service/user", function (data) {
+        $.get("/about/session", function (data) {
+            if (data.prevHere) {
+                $("h2").text("Welcome Back");
+            } else {
+                $("h2").text("Welcome");
+            }
+        }, "json");
+        $("ul").html('');
         if (data.serverAvail) {
-            $("ul").html('<li class="send"><a href="/service/send">Sender</a></li>');
+            $("ul").append('<li class="send"><a href="/service/send">Sender</a></li>');
         }
         $("ul").append('<li class="client"><a href="/service/rec">Client</a></li>');
+        var socket = io("/user/index");
+        socket.on("change", function (data) {
+            $("ul").html('');
+            if (data) {
+                $("ul").append('<li class="send"><a href="/service/send">Sender</a></li>');
+            }
+            $("ul").append('<li class="client"><a href="/service/rec">Client</a></li>');
+        });
     }, "json");
 });
