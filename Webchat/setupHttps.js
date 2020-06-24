0bf64861375d001ea2ba9c1f0f053cba2fdb6ded
sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-24 12:01:17 +0200

    .

--- Webchat/setupHttps.js
+++ Webchat/setupHttps.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
 const data = fs.readFileSync(__dirname+"/app.js", "utf8");
-fs.writeFileSync(__dirname+"/app.js", "utf8",data.split(`false;/////////////////`).join(`true;/////////////////`));
+fs.writeFileSync(__dirname+"/app.js",data.split(`false;/////////////////`).join(`true;/////////////////`),"utf8");
