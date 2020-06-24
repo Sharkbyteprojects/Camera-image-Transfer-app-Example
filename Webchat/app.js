9e57f3214c1e1839be2dc37e5a97428911d01118
sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-24 08:39:59 +0200

    Change Some little things

--- Webchat/app.js
+++ Webchat/app.js
@@ -1,11 +1,13 @@
 const expr = require("express");
+const events = require('events');
 const http = require("http");
 const morgan = require("morgan");
 const helmet = require("helmet")();
 const app = expr();
 app.use(morgan("tiny"));
 app.use(helmet);
-app.use(expr.static(__dirname+'/p'))
+app.use(expr.static(__dirname + '/p'));
+const changeServ = new events.EventEmitter();
 const pars = [
     {
         "path": "/",
@@ -25,20 +27,36 @@ for (let xx of pars) {
         res.sendFile(`${__dirname}/views/${xx.file}`);
     });
 }
+let userhere = false;
+app.get("/service/user", (req, res) => {
+    res.json({ "serverAvail": !userhere });
+});
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 const serverNamespace = io.of('/server');
 const clientNamespace = io.of('/client');
+const indexNamespace = io.of("/user/index");
+indexNamespace.on('connection', (socket) => {
+    changeServ.on('change', () => {
+        socket.emit("change", !userhere);
+    });
+});
 serverNamespace.on('connection', (socket) => {
     socket.emit("welcome", true);
     console.log('a user connected');
     socket.on("imagestream", (data) => {
-        if (data != "data:,") {
+        if (data !== "data:,") {
+            if (!userhere) {
+                userhere = true;
+                changeServ.emit('change');
+            }
             clientNamespace.emit("imagestream", data);
         }
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        userhere = false;
+        changeServ.emit('change');
     });
 });
 clientNamespace.on('connection', (socket) => {
---
+++ Webchat/p/index.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $.get("/service/user", function(data) {
+        if (data.serverAvail) {
+            $("ul").html('<li class="send"><a href="/service/send">Sender</a></li>');
+        }
+        $("ul").append('<li class="client"><a href="/service/rec">Client</a></li>');
+    }, "json");
+});
