38b521d059a323146273ff0b5939e729824b7ff5
sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-24 11:53:59 +0200

    REN

--- Webchat/changeToHttps.sh
+++ Webchat/changeToHttps.sh
@@ -1,3 +1,5 @@
+#USE IT:
+#		curl --ssl-no-revoke https://raw.githubusercontent.com/Sharkbyteprojects/webchat/master/Webchat/changeToHttps.sh|sh
 openssl genrsa -out key.pem 4096
 openssl req -new -x509 -key key.pem -out cert.pem -days 9999 -subj /CN=localhost
 node setupHttps.js
