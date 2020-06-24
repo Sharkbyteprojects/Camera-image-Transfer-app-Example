2369a79576d9adb8401b39dc4a814d86636524a8
sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-24 12:07:32 +0200

    -

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### You need to have installed:
+- npm
+- node.js
+- curl
+- git
---
+++ runht.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Sharkbyteprojects/webchat.git
+cd ./webchat/Webchat
+npm i&&node app.js
